956af8d70f78ea3dfc787e481054f35e9075a8ce
Andriy <andriy_s@idapGit.(none)>
2014-06-17 17:18:42 +0300

    Added 'validatable' module to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :confirmable
+         :recoverable, :rememberable, :trackable, :confirmable, :validatable
   validates :email, :uniqueness => true, :presence => true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :name, :presence => true
-
+  validates :name, :password, :presence => true
 end
